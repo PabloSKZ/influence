83b7f3ad12480baed7a7b3abf4bd71d05b07c64f
Nazaninf24 <nazaninf24@gmail.com>
2020-03-17 17:36:58 +0100

    add

--- db/schema.rb
+++ db/schema.rb
@@ -21,8 +21,11 @@ ActiveRecord::Schema.define(version: 2020_03_17_154025) do
     t.string "picture"
     t.text "description"
     t.decimal "price"
+    t.string "status"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
   create_table "projects", force: :cascade do |t|
