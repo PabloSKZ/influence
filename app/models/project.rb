31a1f486b324942ff28f76051f877c8b17eae886
Stovenn <jonathan.sylvestre.pro@gmail.com>
2020-03-17 23:33:28 +0100

    Table relation MVP final

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,3 +1,4 @@
 class Project < ApplicationRecord
   has_many :adverts
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_17_190204) do
+ActiveRecord::Schema.define(version: 2020_03_17_221749) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,7 +48,9 @@ ActiveRecord::Schema.define(version: 2020_03_17_190204) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "advert_id"
+    t.bigint "user_id"
     t.index ["advert_id"], name: "index_projects_on_advert_id"
+    t.index ["user_id"], name: "index_projects_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -70,4 +72,5 @@ ActiveRecord::Schema.define(version: 2020_03_17_190204) do
 
   add_foreign_key "adverts", "users"
   add_foreign_key "projects", "adverts"
+  add_foreign_key "projects", "users"
 end
