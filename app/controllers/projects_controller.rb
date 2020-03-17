e8a9ce6d1cdbb758fcbd426f521648c33c976f09
Chelton Ajax <chelton1506@gmail.com>
2020-03-17 20:08:43 +0100

    merge save

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -4,17 +4,21 @@ class ProjectsController < ApplicationController
   end
 
   def show
+    # if current_user == nil
+      # redirect_to root_path
+    # end
     @project = Project.find(params[:id])
     session[:id] = @project.id
   end
 
   def new
+    @project = Project.new
     puts params
     puts "OH ZEBI FONCTIONNE LA NON"
   end
 
   def create
-    @project = Project.new(title: params[:title], content: params[:description])
+    @project = Project.new(title: params[:title], description: params[:description], img_url: '#')
       if @project.save
         flash[:notice] = "Votre projet a bien été créé."
         redirect_to root_path
@@ -28,5 +32,11 @@ class ProjectsController < ApplicationController
   end
 
   def update
+    @project = Project.find(params[:id])
+  if @project.update(title: params[:title], description: params[:description])
+    redirect_to @project
+  else
+    render :edit
+  end
   end
 end
