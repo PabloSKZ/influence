59a444079af974d5c8e89a4ef4520c5847dc4806
Chelton Ajax <chelton1506@gmail.com>
2020-03-17 16:46:30 +0100

    merge save

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,32 @@
+class ProjectsController < ApplicationController
+  def index
+    @projects = Project.all
+  end
+
+  def show
+    @project = Project.find(params[:id])
+    session[:id] = @project.id
+  end
+
+  def new
+    puts params
+    puts "$"*60
+  end
+
+  def create
+    @project = Project.new(title: params[:title], content: params[:description])
+      if @project.save
+        flash[:notice] = "Votre projet a bien été créé."
+        redirect_to root_path
+      else
+        @errors = @project.errors
+        render new_project_path
+      end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+end
