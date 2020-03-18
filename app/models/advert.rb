28867502e41326fa5c88ee3e9f79095f94ccc9e2
Stovenn <jonathan.sylvestre.pro@gmail.com>
2020-03-18 11:59:20 +0100

    Validation for mandatory fields (advert & project)

--- app/models/advert.rb
+++ app/models/advert.rb
@@ -2,5 +2,5 @@ class Advert < ApplicationRecord
   belongs_to :user
   has_many :adverts_projects_joins
   has_many :projects, through: :adverts_projects_joins
-  # validates :title, :description, :price, :tag, presence: true --- a decommenter lorsque pret a utiliser
+  validates :title, :description, :price, :tag, presence: true
 end
