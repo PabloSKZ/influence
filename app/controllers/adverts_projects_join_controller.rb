be4a536a4aeaddc83d9b79753866ee5b57d7fd73
PabloSKZ <pablo.eskenazi@gmail.com>
2020-03-19 16:53:47 +0100

    join project ok

--- app/controllers/adverts_projects_join_controller.rb
+++ app/controllers/adverts_projects_join_controller.rb
@@ -1,7 +1,7 @@
 class AdvertsProjectsJoinController < ApplicationController
     def create
         puts params
-        @ad_pro_join = AdvertsProjectsJoin.create(project_id: params[:project_id], advert_id: params[:token])
+        @ad_pro_join = AdvertsProjectsJoin.create(project_id: params[:project_id], advert_id: params[:format])
         redirect_to root_path
     end
 end
