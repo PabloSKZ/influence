9e305162bc838bf07abc8d0434836f98aadb085c
PabloSKZ <pablo.eskenazi@gmail.com>
2020-03-19 17:51:43 +0100

    invite advert to project ok

--- app/controllers/adverts_projects_join_controller.rb
+++ app/controllers/adverts_projects_join_controller.rb
@@ -1,7 +1,7 @@
 class AdvertsProjectsJoinController < ApplicationController
     def create
         puts params
-        @ad_pro_join = AdvertsProjectsJoin.create(project_id: params[:project_id], advert_id: params[:format])
+        @ad_pro_join = AdvertsProjectsJoin.create(project_id: params[:format], advert_id: params[:project_id])
         redirect_to root_path
     end
 end
