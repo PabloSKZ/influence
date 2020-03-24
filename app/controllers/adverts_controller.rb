638175a16342b9b422e725da7cb9517cf18757c3
Cheltonne <chelton1506@gmail.com>
2020-03-24 12:34:20 -0700

    bugfix

--- app/controllers/adverts_controller.rb
+++ app/controllers/adverts_controller.rb
@@ -18,7 +18,7 @@ class AdvertsController < ApplicationController
     @advert = Advert.new(title: params[:advert][:title],tag: params[:advert][:tag],description: params[:advert][:description],price: params[:advert][:price],link_field: params[:advert][:link_field])
     @advert.user_id = current_user.id
     @advert.picture = "https://images.assetsdelivery.com/compings_v2/kritchanut/kritchanut1406/kritchanut140600093.jpg"
-    
+    @errors = @advert.errors
     if @advert.save
       flash[:success] = "Votre annonce a bien été créée."
       redirect_to advert_path(@advert)
