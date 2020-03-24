90d7aa4beb22aed1d9655cbf971b3e68bd60136b
Cheltonne <chelton1506@gmail.com>
2020-03-24 04:57:16 -0700

    installed Cancancan and set it up

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -6,19 +6,23 @@ class Ability
   def initialize(user)
     alias_action :create, :read, :update, :destroy, to: :crud
     alias_action :read, :update, :destroy, to: :rud
+    alias_action :update, :destroy, to: :rud
     
     if user
       if user.user_type == 'influencer'
         can :create, Project
-        can :rud, Project, user_id: user.id
+        can :read, Project
+        can :ud, Project, user_id: user.id
         can :read, Advert
       elsif user.user_type == 'freelance'
         can :create, Advert
-        can :rud, Advert, user_id: user.id
+        can :read, Advert
+        can :ud, Advert, user_id: user.id
         can :read, Project
       end
     else
+      can :read, Advert
     end
   end
-  
+
 end
